fbf84a19b592fe9675aa949e58a1ff6c5534b0f5
Robert Trojan <robert9620@gmail.com>
2018-10-27 20:22:24 +0200

    all data is ready

--- venv/Main.py
+++ venv/Main.py
@@ -1,17 +1,26 @@
 import numpy as np
 import pandas as pd
+import scipy.spatial as sp
 
-class kNN:
-    def __init__(self, k, listaUczaca):
+
+class ClasskNN:
+    def __init__(self, k, lista_uczaca):
         self.k = k
+        self.lista_uczaca = lista_uczaca
 
-    def predict(self, listaZObiektami):
+    def predict(self, lista_z_obiektami):
         return ""
 
-    def score(self, listaZObiektami, listaEtykiet):
+    def score(self, lista_z_obiektami, lista_etykiet):
         return ""
 
 
-listaUczaca = pd.read_csv("data-learning.csv")
-listaTestujaca = pd.read_csv("data-test.csv")
-print(listaTestujaca)
+listaUczaca = np.array(pd.read_csv("data-learning.csv", header=None))
+listaTestujaca = np.array(pd.read_csv("data-test.csv", header=None))
+
+listaUczacaBezEtykiet = np.delete(listaUczaca, len(listaUczaca[0])-1, axis=1)
+listaTestujacaBezEtykiet = np.delete(listaTestujaca, len(listaTestujaca[0])-1, axis=1)
+
+#print(sp.distance.euclidean(listaUczaca[0], listaTestujaca[0]))
+
+artificialIntelligence = ClasskNN(3, listaUczaca)
