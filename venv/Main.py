1c4e67f6dd1ba8381d894300907ef6e2cbb8ab3e
Robert Trojan <robert9620@gmail.com>
2018-10-27 19:21:45 +0200

    reading data from csv is ready

--- venv/Main.py
+++ venv/Main.py
@@ -1,11 +1,17 @@
-import numpy
+import numpy as np
+import pandas as pd
 
 class kNN:
-    def __init__(self, k):
+    def __init__(self, k, listaUczaca):
         self.k = k
 
-    def predict(self):
+    def predict(self, listaZObiektami):
         return ""
 
-    def score(self):
+    def score(self, listaZObiektami, listaEtykiet):
         return ""
+
+
+listaUczaca = pd.read_csv("data-learning.csv")
+listaTestujaca = pd.read_csv("data-test.csv")
+print(listaTestujaca)
